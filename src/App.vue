29261681c6878e3c45b2e42cb484774a815e449e
Colin Maudry <colin@maudry.com>
2020-07-06 21:53:02 +0200

    Improved nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <nav id="nav">
-      <router-link to="/a-propos">À propos</router-link>
-      <router-link to="/">Dictionnaire</router-link>
+      <ul>
+        <li><router-link to="/a-propos">À propos</router-link></li>
+        <li><router-link to="/">Dictionnaire</router-link></li>
+      </ul>
     </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <style>
-
+nav a.router-link-exact-active {
+  display:none;
+}
 </style>
 
 <script>
