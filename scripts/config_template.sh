f076ed4a128d5e33f8d3a69aa71b9f0ffdeeaab4
Colin Maudry <colin@maudry.com>
2020-07-25 12:20:15 +0200

    Ajout de l'id dans les champs de base, et de l'id de la ressource dgfr

--- scripts/config_template.sh
+++ scripts/config_template.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-headers=term,definition,source,url_source,explanation,source_explanation,url_source_explanation
+headers=id,term,definition,source,url_source,explanation,source_explanation,url_source_explanation
 headersDev=$headers,term_status,explanation_status
 
 
 contributionsUrl=https://docs.google.com/spreadsheets/d/1kEJzE-8nCBZakKWptfrc5HhuctOEnMYy8AgxGz6dNW4/export\?format\=csv\&id\=1kEJzE-8nCBZakKWptfrc5HhuctOEnMYy8AgxGz6dNW4\&gid\=0
 slashtradUrl=https://docs.google.com/spreadsheets/d/1kID2eoZk4iwW7JCUKIh_hmvJYZjYDwKuE93hICIjQ7U/export\?format\=csv\&id\=1kID2eoZk4iwW7JCUKIh_hmvJYZjYDwKuE93hICIjQ7U\&gid\=676342226
+
+resource_sigle_id=0fca6fc3-4919-458d-9c81-a2cf54a801ba
 
 datagouvfr_API_key=
